3df7fe0abee3ad0b0a9c14024000a57de955f7bb
Ryan S <skidad75@gmail.com>
2024-05-20 14:34:18 -0700

    fda1.6

--- fda.py
+++ fda.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import streamlit as st
+from json2html import json2html
 
 st.set_page_config(page_title="FDA API Feeds, check out device data feeds from the Food and Drug Administration", layout="centered", initial_sidebar_state="auto", menu_items=None)
 st.title("FDA Data")
@@ -19,7 +20,6 @@ headers = {
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
-
-#print the request, you can also import a web assembly module using python flask or my personal fav streamlit st functions
-st.write(json.dumps(response))
+html = json2html.convert(json=response)
+st.write(html, unsafe_allow_html=True)
 
