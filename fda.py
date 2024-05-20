04d8c2d0d005c5f3c54c9e5fcc13940ac305e03f
Ryan S <skidad75@gmail.com>
2024-05-20 14:13:27 -0700

    fda9

--- fda.py
+++ fda.py
@@ -21,6 +21,6 @@ response = requests.request("GET", url, headers=headers, data=payload)
 
 #print the request, you can also import a web assembly module using python flask or my personal fav streamlit st functions
 
-data = response
+data = json.loads(response.request())
 
 st.json(data)
